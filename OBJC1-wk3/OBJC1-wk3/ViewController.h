41883e00078f4918ed9a4c896d0f2bc003961d4f
Shae <shae@fullsail.com>
2012-04-19 20:28:30 -0700

    prom 3 Upload

---
+++ OBJC1-wk3/OBJC1-wk3/ViewController.h
@@ -0,0 +1,33 @@
+//
+//  ViewController.h
+//  OBJC1-wk3
+//
+//  Created by Shae Klusman on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController{
+    
+    int addTotal;
+    int red;
+    int blue;
+    NSString *String1;
+    NSString *String2;
+    NSNumber *Total;
+    NSString *message;
+    NSString *TotalString;
+    NSString *alertString;
+    NSString *sameYES;
+    NSString *sameNO;
+    NSString *myNewString;
+}
+
+- (int) ADD:(int)numberX with:(int)numberY;
+- (BOOL) COMPARE:(int)numberX with:(int)numberY;
+- (NSString*) APPEND:(NSString*)str1 with:(NSString*)str2;
+- (void) displayAlertWithString:(NSString*)StringIN;
+
+@end
---
+++ OBJC1-wk3/OBJC1-wk3/ViewController.m
@@ -0,0 +1,98 @@
+//
+//  ViewController.m
+//  OBJC1-wk3
+//
+//  Created by Shae Klusman on 4/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+//
+
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    
+    red = 30;
+    blue = 32;
+    addTotal = [self ADD:red with:blue];
+    Total = [[NSNumber alloc] initWithInt:addTotal];
+    message = [NSString stringWithFormat:@"The numbers %d and %d added together is ", red, blue];
+    TotalString = [Total stringValue];
+    alertString = [self APPEND:message with:TotalString]; 
+    String1 = @"My 1st String. ";
+    String2 = @"My 2nd String. ";
+    myNewString = [self APPEND:String1 with:String2];
+    
+    
+    
+    [self displayAlertWithString:alertString];  
+    [self displayAlertWithString:myNewString];
+    [self COMPARE:red with:blue];   
+    
+    [super viewDidLoad];
+}
+
+
+
+-(int)ADD:(int)numberX with:(int)numberY{
+    return numberX + numberY;
+}
+
+
+-(BOOL)COMPARE:(int)numberX with:(int)numberY{
+    
+    sameYES = [NSString stringWithFormat:@"The numbers %d and %d are the same!", red, blue ];
+    sameNO = [NSString stringWithFormat:@"The numbers %d and %d are NOT the same!", red, blue ]; 
+    
+    if (numberX == numberY) {
+        [self displayAlertWithString:sameYES];        
+    }else{
+        [self displayAlertWithString:sameNO]; 
+    }
+    return 0;
+}
+
+
+-(NSString *)APPEND:(NSString*)str1 with:(NSString*)str2{
+    NSMutableString *appendedString = [NSMutableString stringWithString:str1];
+    NSString *string = [appendedString stringByAppendingString:str2];
+    return string;
+}
+
+
+-(void)displayAlertWithString:(NSString *)StringIN{
+    UIAlertView *alert = [[UIAlertView alloc] 
+      initWithTitle:@"-Attention-" 
+      message:StringIN 
+      delegate:nil 
+      cancelButtonTitle:@"Ok Fine!" 
+      otherButtonTitles:nil];
+    [alert show];
+}
+
+
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    } else {
+        return YES;
+    }
+}
+
+@end
